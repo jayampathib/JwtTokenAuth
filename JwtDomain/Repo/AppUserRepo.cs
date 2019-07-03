d831445cdcdba6bcf92f52b6c6bfcfdf81ed6e21
W. M. JAYAMPATHI BANDARA WEERASINGHE <wmjb.weerasinghe@gmail.com>
2019-07-04 00:39:53 +0800

    5.4 Add Repository to retrieve App USer

---
+++ JwtDomain/Repo/AppUserRepo.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JwtDomain.Repo
+{
+    public interface IAppUserRepo
+    {
+        AppUser GetAppUser(string UserName, string Password);
+    }
+
+    public class AppUserRepo : IAppUserRepo
+    {
+        private readonly JwtModuleDbContext _JwtModuleDbContext;
+        public AppUserRepo(JwtModuleDbContext dbContext)
+        {
+            _JwtModuleDbContext = dbContext;
+        }
+        public AppUser GetAppUser(string UserName, string Password)
+        {
+            try
+            {
+                return _JwtModuleDbContext.
+                    AppUsers
+                    .Include(a => a.AppUserClaims) //.Select(c => new AppUserClaim() { AppUserClaimId=c.AppUserClaimId, AppUserId=c.AppUserId, ClaimType =c.ClaimType, ClaimValue=c.ClaimValue }))
+                    .Where(
+                  u => u.UserName.ToLower() == UserName.ToLower()
+                  && u.Password == Password).FirstOrDefault()
+                  ;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            // Attempt to validate user
+
+
+        }
+    }
+}
