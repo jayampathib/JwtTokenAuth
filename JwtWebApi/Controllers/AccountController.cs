5a96b2425966566dc64f6d7ecadb46681ca0a19d
W. M. JAYAMPATHI BANDARA WEERASINGHE <wmjb.weerasinghe@gmail.com>
2019-07-04 00:48:25 +0800

    6.3 Add Login Method

--- JwtWebApi/Controllers/AccountController.cs
+++ JwtWebApi/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JwtWebApi.BLL;
 using JwtWebApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,17 +13,33 @@ namespace JwtWebApi.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+        ISecurityManager _ISecurityManager;
+        public AccountController(ISecurityManager iSecurityManager)
+        {
+            _ISecurityManager = iSecurityManager;
+        }
         // GET api/<controller>/5
         [HttpGet]
         public string Get()
         {
             return "value";
         }
+
         [HttpPost("Login")]
         public IActionResult Login([FromBody]AppUserVM user)
         {
-            IActionResult ret   = StatusCode(StatusCodes.Status200OK, user);
+            IActionResult ret = null;
+            var auth = _ISecurityManager.ValidateUser(user.UserName, user.Password);
 
+            if (auth.IsAuthenticated)
+            {
+                ret = StatusCode(StatusCodes.Status200OK, auth);
+            }
+            else
+            {
+                ret = StatusCode(StatusCodes.Status404NotFound,
+                                 "Invalid User Name/Password.");
+            }
             return ret;
         }
     }
