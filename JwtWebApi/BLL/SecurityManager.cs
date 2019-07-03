df87865459e92416050fa4107720aaa86a259f0b
W. M. JAYAMPATHI BANDARA WEERASINGHE <wmjb.weerasinghe@gmail.com>
2019-07-04 00:45:16 +0800

    6.2 Add Security Manage BL class to validate username and password

---
+++ JwtWebApi/BLL/SecurityManager.cs
@@ -0,0 +1,73 @@
+﻿using FileServer.Model;
+using JwtDomain;
+using JwtDomain.Repo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtWebApi.BLL
+{
+    public interface ISecurityManager
+    {
+        AppUserAuthVM ValidateUser(string userName, string passWord);
+        AppUserAuthVM BuildUserAuthObject(AppUser authUser);
+    }
+
+    public class SecurityManager : ISecurityManager
+    {
+        private readonly IAppUserRepo _IAppUserRepo;
+        public SecurityManager(IAppUserRepo iAppUserRepo)
+        {
+            _IAppUserRepo = iAppUserRepo;
+        }
+        public AppUserAuthVM ValidateUser(string userName, string passWord)
+        {
+            AppUserAuthVM ret = new AppUserAuthVM();
+
+            AppUser authUser = _IAppUserRepo.GetAppUser(userName, passWord);
+
+            if (authUser != null)
+            {
+                // Build User Security Object
+                ret = BuildUserAuthObject(authUser);
+            }
+
+            return ret;
+        }
+
+
+
+        public AppUserAuthVM BuildUserAuthObject(AppUser authUser)
+        {
+            AppUserAuthVM ret = new AppUserAuthVM();
+            List<AppUserClaim> claims = new List<AppUserClaim>();
+
+            // Set User Properties
+            ret.UserName = authUser.UserName;
+            ret.IsAuthenticated = true;
+            ret.BearerToken = new Guid().ToString();
+
+            // Get all claims for this user
+            claims = authUser.AppUserClaims.ToList();
+
+            // Loop through all claims and 
+            // set properties of user object
+            foreach (AppUserClaim claim in claims)
+            {
+                try
+                {
+                    // TODO: Check data type of ClaimValue
+                    typeof(AppUserAuthVM).GetProperty(claim.ClaimType)
+                      .SetValue(ret, Convert.ToBoolean(claim.ClaimValue), null);
+                }
+                catch
+                {
+                }
+            }
+
+            return ret;
+        }
+    }
+}
+}
---
+++ JwtWebApi/Models/AppUserAuthVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileServer.Model
+{
+	public class AppUserAuthVM
+	{
+		public AppUserAuthVM() : base()
+		{
+			UserName = "Not authorized";
+			BearerToken = string.Empty;
+		}
+
+		public string UserName { get; set; }
+		public string BearerToken { get; set; }
+		public bool IsAuthenticated { get; set; }
+		public bool CanAccessProducts { get; set; }
+		public bool CanAddProduct { get; set; }
+		public bool CanSaveProduct { get; set; }
+		public bool CanAccessCategories { get; set; }
+		public bool CanAddCategory { get; set; }
+	}
+}
