eb9ecfe133ed1868c9d61c274b280e7a2ec2bff3
W. M. JAYAMPATHI BANDARA WEERASINGHE <wmjb.weerasinghe@gmail.com>
2019-07-04 00:14:35 +0800

    6.1 add Login Action

--- JwtWebApi/Controllers/AccountController.cs
+++ JwtWebApi/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JwtWebApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,6 +17,13 @@ namespace JwtWebApi.Controllers
         public string Get()
         {
             return "value";
+        }
+        [HttpPost("Login")]
+        public IActionResult Login([FromBody]AppUserVM user)
+        {
+            IActionResult ret   = StatusCode(StatusCodes.Status200OK, user);
+
+            return ret;
         }
     }
 }
---
+++ JwtWebApi/Models/AppUserVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtWebApi.Models
+{
+    public class AppUserVM
+    {
+         
+         
+        [MaxLength(30)]
+        [Required]
+        public string UserName { get; set; }
+        [MaxLength(30)]
+        [Required]
+        public string Password { get; set; }
+
+         
+    }
+}
