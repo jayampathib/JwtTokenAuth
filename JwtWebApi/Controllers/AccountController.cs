2489e4b2e339f5fe94538b1492df3312b0b8c97e
W. M. JAYAMPATHI BANDARA WEERASINGHE <wmjb.weerasinghe@gmail.com>
2019-07-04 00:00:09 +0800

    1.0 Create  Initial Web Api Project

---
+++ JwtWebApi/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        // GET api/<controller>/5
+        [HttpGet]
+        public string Get()
+        {
+            return "value";
+        }
+    }
+}
