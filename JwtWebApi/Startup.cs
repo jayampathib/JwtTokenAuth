f000252ce8a61a5915217df77ec6452429978da2
W. M. JAYAMPATHI BANDARA WEERASINGHE <wmjb.weerasinghe@gmail.com>
2019-07-04 00:30:06 +0800

    5.2 Add DbContext configuration to startup project

--- JwtWebApi/Startup.cs
+++ JwtWebApi/Startup.cs
@@ -6,16 +6,26 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using JwtDomain;
+using Microsoft.Extensions.Configuration;
 
 namespace JwtWebApi
 {
     public class Startup
     {
+        private readonly IConfiguration _IConfiguration;
+        public Startup(IConfiguration iConfiguration)
+        {
+            _IConfiguration = iConfiguration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<JwtModuleDbContext>(opt => 
+                    opt.UseSqlServer(_IConfiguration.GetConnectionString("sqlConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
