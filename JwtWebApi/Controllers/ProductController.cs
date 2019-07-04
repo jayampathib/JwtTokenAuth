b49d5b6e7a6086bbe29bc9839930c28ac17be241
W. M. JAYAMPATHI BANDARA WEERASINGHE <wmjb.weerasinghe@gmail.com>
2019-07-04 21:14:49 +0800

    6.5 Add controller with Authorize attribute

---
+++ JwtWebApi/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        public string Get()
+        {
+            return "product value";
+        }
+    }
+}
